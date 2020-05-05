cb4918bc0a97ced8c94a543286d25c62bf9b9c42
oldrookie-engineer <paburofu0023@icloud.com>
2020-05-05 14:45:15 +0900

    タスクの編集機能を搭載

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -17,15 +18,17 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-
   end
 
   def update
-
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'タスクを編集しました！'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -34,6 +37,10 @@ class TasksController < ApplicationController
   private
   def task_params
     params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
