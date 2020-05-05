b3c2564a960d81c6405d10191934ba07139f3579
oldrookie-engineer <paburofu0023@icloud.com>
2020-05-05 17:23:42 +0900

    システムスペックテスト完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: 'タスク作成しました！'
+      redirect_to task_path(@task.id), notice: 'タスク作成しました！'
     else
       render :new
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true
-  validates :content, presence: true, length: {in: 0..140}
+  # validates :title, presence: true
+  # validates :content, presence: true, length: {in: 0..140}
 end
