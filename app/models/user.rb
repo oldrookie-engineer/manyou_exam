fdfed71e7b8346c9c56a6ec8b1cf60b234dc6e9d
oldrookie-engineer <paburofu0023@icloud.com>
2020-05-12 21:23:43 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence:true
-  validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence:true, length: {maximum: 30}
+  validates :email, presence:true, length: {maximum: 255},
+  uniqueness:true,
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
   has_secure_password
   validates :password, presence:true, unless: :password_digest
   before_destroy :admin_destroy_check
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(
+      email: 'aa@bb.com'
+    )
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'emailが空ならバリデーションが通らない' do
+    user = User.new(email: '')
+    expect(user).not_to be_valid
+  end
 end
