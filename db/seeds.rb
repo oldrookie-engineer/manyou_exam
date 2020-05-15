900a3a2aa4777e1030dac2ddc967f7fefc7cbbad
oldrookie-engineer <paburofu0023@icloud.com>
2020-05-15 18:34:31 +0900

    seedデータ修正④

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,43 @@ User.create!(
   password_confirmation: "adminmaster",
   admin: true)
 
-10.times do |n|
+3.times do |n|
   User.create!(
     name: "test#{n}",
     email: "example#{n}@test.com",
     password: "password#{n}",
-    password_digest: "password#{n}"
+    password_confirmation: "password#{n}"
   )
 end
 
-10.times do |n|
   Task.create!(
-    title: "title#{n}",
-    content: "content#{n}",
+    title: "課題１",
+    content: "万葉step1",
+    status: "完了",
+    priority: "高",
+    timelimit: 2020-05-08,
+    user_id:1
+  )
+
+  Task.create!(
+    title: "課題２",
+    content: "万葉step2",
     status: "着手中",
     priority: "高",
-    timelimit: 2020-05-15
+    timelimit: 2020-05-10,
+    user_id:2
   )
-end
 
-10.times do |n|
+  Task.create!(
+    title: "課題３",
+    content: "万葉step3",
+    status: "未着手",
+    priority: "高",
+    timelimit: 2020-05-15,
+    user_id:3
+  )
+
+
+5.times do |n|
   Label.create!(name:"label: #{n}")
 end
